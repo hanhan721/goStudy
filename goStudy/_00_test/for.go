a70f2115d00300c4e07dc03a4e55c036337ad5f6
ray <ray>
2024-10-18 09:27:38 +0800

    for练习

--- goStudy/_00_test/for.go
+++ goStudy/_00_test/for.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 )
 
+// 简单的平方根方法，只求出整数位
 func Sqrt(x float64) float64 {
-	z := 1
+	z := 1.0
 	for {
 		if z*z > x {
-
+			return z - 1
 		}
 		z++
 	}
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(10001))
 }
