92e56f4fcaeddd7cbe222ab52ac1f050fa5e8087
Ray <491514512@qq.com>
2024-10-21 23:29:58 +0800

    reflect

--- goStudy/_12_reflect/main.go
+++ goStudy/_12_reflect/main.go
@@ -11,6 +11,12 @@ type User struct {
 	age  int
 }
 
+// func (u *User) Call() {  //使用指针反射不到方法
+func (u User) Call() {
+	fmt.Println("User is called....")
+	fmt.Printf("%v\n", u)
+}
+
 func main() {
 	u := User{
 		id:   123,
@@ -19,9 +25,24 @@ func main() {
 	}
 	uType := reflect.TypeOf(u)
 	uValue := reflect.ValueOf(u)
-	fmt.Println(uType)
-	fmt.Println(uValue)
+	fmt.Println("uType: ", uType)
+	fmt.Println("uValue: ", uValue)
 	fmt.Println(u)
+	fmt.Println(reflect.TypeOf(u.id))
+	fmt.Println(reflect.ValueOf(u.id))
 	fmt.Println(reflect.TypeOf(uValue))
+	fmt.Println("-------------------")
+	//反射获取字段以及值
+	for i := 0; i < uType.NumField(); i++ {
+		field := uType.Field(i)
+		value := uValue.Field(i)
+		fmt.Println(field, "--", value)
+	}
+	fmt.Println("---------------------")
+	//反射获取方法
+	for i := 0; i < uType.NumMethod(); i++ {
+		method := uType.Method(i)
+		fmt.Println(method)
+	}
 
 }
